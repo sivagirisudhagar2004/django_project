520320115acdb9d92d1abf438ab3a0d2f9f38ad0
sivagirisudhagar2004 <sivagirisudhagar@gmail.com>
2026-09-21 15:38:37 +0530

    Updated

--- my_django_app/My_Application/views.py
+++ my_django_app/My_Application/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home (request):
+    msg = " <h1>Welcome to Django Framework</h1>"
+    return HttpResponse(msg)
+
+def index(request):
+    return HttpResponse("<h1>This is Index page</h1>")
+
+def users(request):
+    return HttpResponse("<h1>This is users page</h1>")
+
+
+
