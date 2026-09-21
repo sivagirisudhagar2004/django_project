390188ec5dbc85c13c65902331d374d8864877c7
sivagirisudhagar2004 <sivagirisudhagar@gmail.com>
2026-09-21 15:41:39 +0530

    Some Updetes

--- my_django_app/my_django_app/urls.py
+++ my_django_app/my_django_app/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from My_Application import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+     path('',views.home),
+    path('Index',views.index),
+    path('Users',views.users)
 ]
